f0bc17233a47114dc0a1b88619817165fe950e13
ChelesteWang <1581446178@qq.com>
2022-07-20 00:19:19 +0800

    Revert "fix: test coverage warning"
    This reverts commit 4b1a465b353b93a3f3ca5c80cb43fd374a4a02d2.

--- packages/arcodesign/components/transition/__test__/index.spec.js
+++ packages/arcodesign/components/transition/__test__/index.spec.js
@@ -1,5 +1,4 @@
 import mountTest from '../../../tests/mountTest';
 import Transition from '..';
-import React from 'react'
 
-mountTest(Transition, 'Transition', { children: <></>, timeout: 2000 });
+mountTest(Transition, 'Transition');
--- packages/arcodesign/tests/mountTest.js
+++ packages/arcodesign/tests/mountTest.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-export default function mountTest(Component, compName, props) {
+export default function mountTest(Component, compName) {
     describe(`${compName} mount and unmount`, () => {
         it(`${compName} could be updated and unmounted without errors`, () => {
-            const wrapper = mount(<Component {...props} />);
+            const wrapper = mount(<Component />);
             expect(() => {
                 wrapper.setProps({});
                 wrapper.unmount();
