72fcc8afbab71ac22503de09f8024a9da61afac0
高辰晶 <gaochenjing@bytedance.com>
2023-01-19 11:44:20 +0800

    feat: form

--- packages/arcodesign/tokens/scripts/generate/index.js
+++
@@ -1,6 +0,0 @@
-const generateToken = require('./generate');
-
-if (process.argv[2]) {
-    const compFilter = (process.argv[3] || '').trim();
-    generateToken({ appName: process.argv[2], outputFilter: compFilter ? compFilter.split(',') : [] });
-}
