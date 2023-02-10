244107fa4764141e8e57e871cad60f19f0592706
黄黄汪 <50308467+huangguang1999@users.noreply.github.com>
2023-02-10 18:00:39 +0800

    fix: `ImagePreview` fix display problems when passing fit layout (#91)
    Co-authored-by: 黄广 <huangguang.1999@bytedance.com>

--- packages/arcodesign/components/image-preview/index.tsx
+++ packages/arcodesign/components/image-preview/index.tsx
@@ -1052,10 +1052,11 @@ const ImagePreview = forwardRef((props: ImagePreviewProps, ref: Ref<ImagePreview
     // @en In iOS when resetting the style, the image will disappear and cause flickering, so put a image at the bottom
     // 优先过渡图，其次用原图
     // @en Prioritize the transition image, followed by the original image
-    function renderImagePlaceholder(src: string, index: number) {
+    function renderImagePlaceholder(src: string, index: number, fitCss?: string) {
         const { originWidth, originHeight, originLeft, originTop, hasOverflow } =
             imagesStatus[index] || {};
         const trans = hasOverflow ? {} : getStyleWithVendor({ transform: 'translateY(-50%)' });
+        const fitObj: CSSProperties | {} = fitCss ? { objectFit: fitCss } : {};
         return system === 'ios' && showPlaceholders[index] && originWidth && originHeight ? (
             <img
                 src={src}
@@ -1065,6 +1066,7 @@ const ImagePreview = forwardRef((props: ImagePreviewProps, ref: Ref<ImagePreview
                     height: originHeight,
                     left: originLeft,
                     top: originTop,
+                    ...fitObj,
                     ...trans,
                 }}
             />
@@ -1148,6 +1150,7 @@ const ImagePreview = forwardRef((props: ImagePreviewProps, ref: Ref<ImagePreview
                                     bottomOverlap: renderImagePlaceholder(
                                         image.fallbackSrc || image.src,
                                         index,
+                                        image.fit || fit,
                                     ),
                                     onLoad: (_, imageEle) => {
                                         imageDomsRef.current[index] = imageEle;
