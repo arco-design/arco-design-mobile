65a51ebee7d6a02ee37423ad2c4fe2eecbd35be8
xiaziqi.sia <xiaziqi.sia@bytedance.com>
2023-08-21 21:17:34 +0800

    test: `Image` test migrate from Enzyme to React Testing Library

--- packages/arcodesign/components/image/__test__/index.spec.js
+++ packages/arcodesign/components/image/__test__/index.spec.js
@@ -1,52 +1,94 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+// import { mount } from 'enzyme';
 import demoTest from '../../../tests/demoTest';
 import mountTest from '../../../tests/mountTest';
 import Image from '..';
-import { mount } from 'enzyme';
+import '@testing-library/jest-dom';
 
 demoTest('image');
 
 mountTest(Image, 'Image');
 
 describe('Image', () => {
+    // it('Image renders correctly', () => {
+    //     const wrapper = mount(
+    //         <Image
+    //             staticLabel
+    //             src="https://sf1-cdn-tos.toutiaostatic.com/obj/arco-mobile/_static_/large_image_1.jpg"
+    //         />,
+    //     );
+    //     expect(wrapper.find('img').length).toBe(1);
+    // });
     it('Image renders correctly', () => {
-        const wrapper = mount(
+        const { getAllByRole } = render(
             <Image
                 staticLabel
                 src="https://sf1-cdn-tos.toutiaostatic.com/obj/arco-mobile/_static_/large_image_1.jpg"
             />,
         );
-        expect(wrapper.find('img').length).toBe(1);
+        expect(getAllByRole('img').length).toBe(1);
     });
+    // it('Fill mode renders correctly', () => {
+    //     const wrapper = mount(
+    //         <Image
+    //             fit="contain"
+    //             staticLabel
+    //             src="https://sf1-cdn-tos.toutiaostatic.com/obj/arco-mobile/_static_/large_image_1.jpg"
+    //         />,
+    //     );
+    //     expect(wrapper.find('img').props().style.objectFit).toBe('contain');
+    // });
     it('Fill mode renders correctly', () => {
-        const wrapper = mount(
+        const { getByRole } = render(
             <Image
                 fit="contain"
                 staticLabel
                 src="https://sf1-cdn-tos.toutiaostatic.com/obj/arco-mobile/_static_/large_image_1.jpg"
             />,
         );
-        expect(wrapper.find('img').props().style.objectFit).toBe('contain');
+        expect(getByRole('img')).toHaveStyle({ objectFit: 'contain' });
     });
+    // it('Rendering correctly while loading', () => {
+    //     const wrapper = mount(
+    //         <Image
+    //             status="loading"
+    //             showLoading={true}
+    //             src="https://sf1-cdn-tos.toutiaostatic.com/obj/arco-mobile/_static_/large_image_1.jpg"
+    //         />,
+    //     );
+    //     expect(wrapper.find('svg').length).toBe(1);
+    // });
     it('Rendering correctly while loading', () => {
-        const wrapper = mount(
+        const { container } = render(
             <Image
                 status="loading"
-                showLoading={true}
+                showLoading
                 src="https://sf1-cdn-tos.toutiaostatic.com/obj/arco-mobile/_static_/large_image_1.jpg"
             />,
         );
-        expect(wrapper.find('svg').length).toBe(1);
+        expect(container.querySelectorAll('svg').length).toBe(1);
     });
+    // it('Failed to load renders correctly', () => {
+    //     const wrapper = mount(
+    //         <Image
+    //             status="error"
+    //             showError={true}
+    //             errorArea={<div>加载失败</div>}
+    //             src="https://sf1-cdn-tos.toutiaostatic.com/obj/arco-mobile/_static_/large_image_1.jpg"
+    //         />,
+    //     );
+    //     expect(wrapper.text()).toEqual('加载失败');
+    // });
     it('Failed to load renders correctly', () => {
-        const wrapper = mount(
+        const { container } = render(
             <Image
                 status="error"
-                showError={true}
+                showError
                 errorArea={<div>加载失败</div>}
                 src="https://sf1-cdn-tos.toutiaostatic.com/obj/arco-mobile/_static_/large_image_1.jpg"
             />,
         );
-        expect(wrapper.text()).toEqual('加载失败');
+        expect(container.textContent).toEqual('加载失败');
     });
-})
+});
