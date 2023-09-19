a53a285e53d7cdceae99baf7d9b9af5147815a98
xiaziqi.sia <xiaziqi.sia@bytedance.com>
2023-09-19 20:26:39 +0800

    feat: enzyme to @testing-library/react for demoTest and mountTest

--- packages/arcodesign/tests/demoTest.js
+++ packages/arcodesign/tests/demoTest.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render } from '@testing-library/react';
 import glob from 'glob';
 import path from 'path';
 
 export default function demoTest(comp) {
     describe(`${comp} demo test`, () => {
         const files = glob.sync(path.resolve(__dirname, `../components/${comp}/demo/*.md`));
-        files.forEach((file) => {
+        files.forEach(file => {
             const filename = file.split('/').slice(-1)[0];
             it(`${comp} demo: ${filename} renders correctly`, () => {
                 const demo = require(file).default;
-                const wrapper = render(React.createElement(demo));
-                expect(wrapper).toMatchSnapshot();
+                const { asFragment } = render(React.createElement(demo));
+                expect(asFragment()).toMatchSnapshot();
             });
         });
     });
 }
-
