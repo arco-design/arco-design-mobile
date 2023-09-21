d37804c6051c90ef2dc3f5c6bf307befbbe3d1c8
tangshuning <tangshuning.tsn@bytedance.com>
2023-09-21 19:26:06 +0800

    feat: 补全slider单测

--- packages/arcodesign/components/slider/__test__/index.spec.js
+++ packages/arcodesign/components/slider/__test__/index.spec.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import { act } from 'react-dom/test-utils';
+import React, { useState } from 'react';
+import '@testing-library/jest-dom';
+import { fireEvent, render } from '@testing-library/react';
 import demoTest from '../../../tests/demoTest';
 import mountTest from '../../../tests/mountTest';
 import Slider from '..';
@@ -8,7 +8,6 @@ import { defaultContext } from '../../context-provider';
 import {
     createStartTouchEventObject,
     createMoveTouchEventObject,
-    mockAddListener,
 } from '../../../tests/helpers/mockEvent';
 
 const prefix = `${defaultContext.prefixCls}-slider`;
@@ -17,27 +16,19 @@ demoTest('slider');
 
 mountTest(Slider, 'Slider');
 
-class Test extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: 20,
-        };
-    }
+function Test(props) {
+    const [value, setValue] = useState(20);
 
-    onChange = value => {
-        this.setState({ value });
+    const onChange = newValue => {
+        setValue(newValue);
     };
 
-    render() {
-        const { value } = this.state;
-        return <Slider value={value} onChange={this.onChange} {...this.props} />;
-    }
+    return <Slider date-testid="slider" value={value} onChange={onChange} {...props} />;
 }
 
 function fakeSliderLineRect(component) {
-    const line = component.find(`.${prefix}-line`).at(0);
-    line.getDOMNode().getBoundingClientRect = jest.fn(() => ({
+    const line = component.querySelector(`.${prefix}-line`);
+    line.getBoundingClientRect = jest.fn(() => ({
         top: 600,
         bottom: 602,
         left: 10,
@@ -48,85 +39,66 @@ function fakeSliderLineRect(component) {
 }
 
 describe('Slider', () => {
-    beforeEach(() => {
-        jest.useFakeTimers();
+    it('render correctly', async () => {
+        const { container } = render(<Test />);
+
+        expect(container.querySelectorAll(`.${prefix}`).length).toBe(1);
+        expect(container.querySelectorAll(`.${prefix}-line`).length).toBe(2);
+        expect(container.querySelectorAll(`.${prefix}-line.is-activated`).length).toBe(1);
+        expect(container.querySelectorAll(`.${prefix}-thumb`).length).toBe(1);
     });
 
-    afterEach(() => {
-        jest.useRealTimers();
-    });
+    it('render range slider correctly', async () => {
+        const { container } = render(<Slider range defaultValue={[15, 30]} />);
+        fakeSliderLineRect(container);
+        expect(container.querySelectorAll(`.${prefix}-thumb`).length).toBe(2);
+        // 测试在范围区间滑动是否正确
+        const line = container.querySelector(`.${prefix}-line`);
+        await fireEvent.touchStart(line, createStartTouchEventObject({ x: 30, y: 1 }));
+        await fireEvent.touchMove(line, createMoveTouchEventObject({ x: 50, y: 1 }));
+        await fireEvent.touchEnd(line, createStartTouchEventObject({ x: 50, y: 1 }));
 
-    it('render correctly', () => {
-        const component = mount(<Test />);
-        fakeSliderLineRect(component);
-        expect(component.state('value')).toBe(20);
-        expect(component.find(`.${prefix}`).length).toBe(1);
-        expect(component.find(`.${prefix}-line`).length).toBe(2);
-        expect(component.find(`.${prefix}-line.is-activated`).length).toBe(1);
-        expect(component.find(`.${prefix}-thumb`).length).toBe(1);
-        component.setState({ value: 30 });
-        component.find(`.${prefix}-thumb`).at(0).simulate('mouseenter');
-        expect(component.state('value')).toBe(30);
-        expect(component.find(`.${prefix}-line.is-activated`).at(0).props().style.width).toBe(
-            '30px',
-        );
-    });
-
-    it('render range slider correctly', () => {
-        const component = mount(<Slider range defaultValue={[15, 30]} />);
-        fakeSliderLineRect(component);
-        component.setProps({ max: 60 });
-        expect(component.find(`.${prefix}-thumb`).length).toBe(2);
-        const style = component.find(`.${prefix}-line.is-activated`).at(0).props().style;
-        expect(style.left).toBe('25px');
-        expect(style.width).toBe('25px');
+        expect(container.querySelector(`.${prefix}-popover-content`)).toHaveTextContent('40');
     });
 
     it('marks slider', () => {
-        const component = mount(<Test max={20} showMarks marks={[0, 5, 10, 15, 20]} />);
-        expect(component.find(`.${prefix}`).hasClass('show-marks')).toBe(true);
-        expect(component.find(`.${prefix}-marks-item`).length).toBe(5);
-        expect(component.find(`.${prefix}-marks-item-label`).length).toBe(5);
-        component.setState({ value: 10 });
-        expect(component.find(`.${prefix}-marks-item`).find('.is-activated').length).toBe(5);
+        const { container } = render(<Test max={20} showMarks marks={[0, 5, 10, 15, 20]} />);
+        expect(container.querySelector(`.${prefix}`).classList.contains('show-marks')).toBe(true);
+        expect(container.querySelectorAll(`.${prefix}-marks-item`).length).toBe(5);
+        expect(container.querySelectorAll(`.${prefix}-marks-item-label`).length).toBe(5);
+        // component.setState({ value: 10 });
+        // expect(component.find(`.${prefix}-marks-item`).find('.is-activated').length).toBe(5);
     });
 
-    it('should select mark when onlyMarkValue is true', () => {
-        const component = mount(
-            <Slider showMarks useMarkOnly marks={[0, 5, 10, 15, 20]} defaultValue={12} />,
-        );
-        fakeSliderLineRect(component);
-        component.setProps({ max: 20 });
-        expect(
-            component.find(`.${prefix}-line.is-activated`).at(0).getDOMNode().getAttribute('style'),
-        ).toContain('height: 2px; left: 0px; width: 50px;');
+    it('touch event', async () => {
+        const { container } = render(<Test />);
+        fakeSliderLineRect(container);
+
+        const line = container.querySelector(`.${prefix}-line`);
+        await fireEvent.touchStart(line, createStartTouchEventObject({ x: 20, y: 1 }));
+        await fireEvent.touchMove(line, createMoveTouchEventObject({ x: 50, y: 1 }));
+        await fireEvent.touchEnd(line, createStartTouchEventObject({ x: 50, y: 1 }));
+
+        expect(container.querySelector(`.${prefix}-line.is-activated`).style.width).toBe('40px');
     });
 
-    it('touch event', () => {
-        const wrapper = mount(
-            <Slider
-                showMarks
-                useMarkOnly
-                max={20}
-                marks={[0, 5, 10, 15, 20]}
-                defaultValue={10}
-                disabled={false}
-            />,
+    it('set the number of segments correctly', async () => {
+        const { container } = render(
+            <Slider showMarks useMarkOnly max={20} marks={[0, 5, 10, 15, 20]} defaultValue={5} />,
         );
-        const map = mockAddListener(wrapper.find(`.${prefix}`));
-        wrapper.setProps({ disabled: true });
-        act(() => {
-            map.touchstart(createStartTouchEventObject({ x: 0, y: 0 }));
-            map.touchmove(createMoveTouchEventObject({ x: 300, y: 0 }));
-        });
-        wrapper
-            .find(`.${prefix}-line`)
-            .at(1)
-            .simulate('touchEnd', createMoveTouchEventObject({ x: 300, y: 0 }));
-        wrapper.update();
+
+        fakeSliderLineRect(container);
+        const line = container.querySelector(`.${prefix}-line`);
+        await fireEvent.touchStart(line, createStartTouchEventObject({ x: 20, y: 1 }));
+        await fireEvent.touchMove(line, createMoveTouchEventObject({ x: 50, y: 1 }));
+        await fireEvent.touchEnd(line, createStartTouchEventObject({ x: 50, y: 1 }));
+
+        expect(container.querySelector(`.${prefix}`).getAttribute('aria-valuenow')).toBe('10');
+        expect(container.querySelector(`.${prefix}-line.is-activated`).style.width).toBe('50px');
     });
+
     it('render tooltip correctly', () => {
-        const component = mount(<Slider showTooltip="always" defaultValue={32} />);
-        expect(component.find(`.${prefix}-popover`).length).toBe(1);
+        const { container } = render(<Slider showTooltip="always" defaultValue={32} />);
+        expect(container.querySelectorAll(`.${prefix}-popover`).length).toBe(1);
     });
 });
