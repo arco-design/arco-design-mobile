c721151889e2b28ba39cf2f1fd01229e102b3023
xiaziqi.sia <xiaziqi.sia@bytedance.com>
2023-10-08 17:46:15 +0800

    feat: update tabs unit test

--- packages/arcodesign/components/tabs/__test__/index.spec.js
+++ packages/arcodesign/components/tabs/__test__/index.spec.js
@@ -1,35 +1,37 @@
 import React, { createRef } from 'react';
-import { act } from 'react-dom/test-utils';
-import { mount } from 'enzyme';
+import { act, fireEvent, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import demoTest from '../../../tests/demoTest';
 import { defaultContext } from '../../context-provider';
 import Tabs from '..';
 import { mockElementProperty } from '../../../tests/helpers/mockElement';
-import { createMoveTouchEventObject, createStartTouchEventObject, mockAddListener } from '../../../tests/helpers/mockEvent';
-
-demoTest('tabs');
+import {
+    createMoveTouchEventObject,
+    createStartTouchEventObject,
+    mockAddListener,
+} from '../../../tests/helpers/mockEvent';
+import '@testing-library/jest-dom';
 
 const prefix = `${defaultContext.prefixCls}-tabs`;
 const prefixTab = `${defaultContext.prefixCls}-tab`;
 
-function indexIsActive(wrapper, index) {
-    return wrapper.find(`.${prefixTab}-cell`).at(index).hasClass('active');
+function indexIsActive(container, index) {
+    return container.querySelectorAll(`.${prefixTab}-cell`)[index].classList.contains('active');
 }
 
-function mockSwipe(map, wrapper, clientXMap) {
+function mockSwipe(map, clientXMap) {
     const { touchstart, touchmove, touchend } = clientXMap;
     act(() => {
-        map.touchstart(createStartTouchEventObject({ x: touchstart, y: 0 }));
-        map.touchmove(createMoveTouchEventObject({ x: touchmove, y: 0 }));
+        fireEvent.touchStart(map, createStartTouchEventObject({ x: touchstart, y: 0 }));
+        fireEvent.touchMove(map, createMoveTouchEventObject({ x: touchmove, y: 0 }));
     });
-    wrapper.find(`.${prefixTab}-pane-container`).simulate('touchEnd', createMoveTouchEventObject({ x: touchend, y: 0 }));
+    fireEvent.touchEnd(map, createMoveTouchEventObject({ x: touchend, y: 0 }));
     act(() => {
         jest.advanceTimersByTime(600);
     });
-    wrapper.update();
 }
 
-function mockContainer(ref, wrapper) {
+function mockContainer(ref) {
     const { dom, bar, updateLayout } = ref.current;
     const { dom: barDom } = bar || {};
     expect(typeof dom).toBe('object');
@@ -44,9 +46,7 @@ function mockContainer(ref, wrapper) {
         updateLayout();
         jest.runOnlyPendingTimers();
     });
-    wrapper.update();
 }
-
 
 describe('Tabs', () => {
     beforeEach(() => {
@@ -61,26 +61,42 @@ describe('Tabs', () => {
         const onChange = jest.fn();
         const onAfterChange = jest.fn();
         const onTabClick = jest.fn();
-        const wrapper = mount(<Tabs
-            tabs={[{ title: 'Title 1' }, { title: 'Title 2' }]}
-            defaultActiveTab={1}
-            onChange={onChange}
-            onAfterChange={onAfterChange}
-            onTabClick={onTabClick}
-            transitionDuration={500}
-            disabled={true}
-            useCaterpillar={true}
-        >
-            <div className="demo-tab-content">Content 1</div>
-            <div className="demo-tab-content">Content 2</div>
-        </Tabs>);
-        expect(indexIsActive(wrapper, 1)).toBe(true);
-        expect(wrapper.find(`.${prefixTab}-cell`)).toHaveLength(2);
-        wrapper.find(`.${prefixTab}-cell`).at(0).simulate('click');
+        const { container, rerender } = render(
+            <Tabs
+                tabs={[{ title: 'Title 1' }, { title: 'Title 2' }]}
+                defaultActiveTab={1}
+                onChange={onChange}
+                onAfterChange={onAfterChange}
+                onTabClick={onTabClick}
+                transitionDuration={500}
+                disabled
+                useCaterpillar
+            >
+                <div className="demo-tab-content">Content 1</div>
+                <div className="demo-tab-content">Content 2</div>
+            </Tabs>,
+        );
+        expect(indexIsActive(container, 1)).toBe(true);
+        expect(container.querySelectorAll(`.${prefixTab}-cell`).length).toBe(2);
+        userEvent.click(container.querySelector(`.${prefixTab}-cell`));
         expect(onTabClick.mock.calls).toHaveLength(0);
         expect(onChange.mock.calls).toHaveLength(0);
-        wrapper.setProps({ disabled: false });
-        wrapper.find(`.${prefixTab}-cell`).at(0).simulate('click');
+        rerender(
+            <Tabs
+                tabs={[{ title: 'Title 1' }, { title: 'Title 2' }]}
+                defaultActiveTab={1}
+                onChange={onChange}
+                onAfterChange={onAfterChange}
+                onTabClick={onTabClick}
+                transitionDuration={500}
+                disabled={false}
+                useCaterpillar
+            >
+                <div className="demo-tab-content">Content 1</div>
+                <div className="demo-tab-content">Content 2</div>
+            </Tabs>,
+        );
+        userEvent.click(container.querySelector(`.${prefixTab}-cell`));
         expect(onTabClick.mock.calls).toHaveLength(1);
         expect(onChange.mock.calls).toHaveLength(1);
         expect(onChange.mock.calls[0]).toEqual([{ title: 'Title 1' }, 0, 'click']);
@@ -95,27 +111,53 @@ describe('Tabs', () => {
     it('should support scroll mode correctly', () => {
         const onChange = jest.fn();
         const ref = createRef();
-        const wrapper = mount(<Tabs
-            tabs={[{ title: 'Title 1' }, { title: 'Title 2' }]}
-            onChange={onChange}
-            transitionDuration={500}
-            disabled={true}
-            ref={ref}
-        >
-            <div style={{ height: 300 }}>Content 1</div>
-            <div style={{ height: 300 }}>Content 2</div>
-        </Tabs>);
+        const { container: wrapper, rerender } = render(
+            <Tabs
+                tabs={[{ title: 'Title 1' }, { title: 'Title 2' }]}
+                onChange={onChange}
+                transitionDuration={500}
+                disabled
+                ref={ref}
+            >
+                <div style={{ height: 300 }}>Content 1</div>
+                <div style={{ height: 300 }}>Content 2</div>
+            </Tabs>,
+        );
         // mock size
         mockElementProperty(window, 'innerHeight', 700);
-        mockContainer(ref, wrapper);
+        mockContainer(ref);
         const map = mockAddListener(window, true);
-        wrapper.setProps({ mode: 'scroll', disabled: false, scrollThrottle: 0 });
+        // wrapper.setProps({ mode: 'scroll', disabled: false, scrollThrottle: 0 });
+        rerender(
+            <Tabs
+                tabs={[{ title: 'Title 1' }, { title: 'Title 2' }]}
+                onChange={onChange}
+                transitionDuration={500}
+                disabled={false}
+                scrollOffset={0}
+                mode="scroll"
+                ref={ref}
+            >
+                <div style={{ height: 300 }}>Content 1</div>
+                <div style={{ height: 300 }}>Content 2</div>
+            </Tabs>,
+        );
         // mock scroll position
-        const firstPaneDom = wrapper.find(`.${prefixTab}-pane`).at(0).getDOMNode();
-        firstPaneDom.getBoundingClientRect = jest.fn(() => ({ top: 100, bottom: 400, left: 0, right: 375 }));
-        const lastPaneDom = wrapper.find(`.${prefixTab}-pane`).at(1).getDOMNode();
-        lastPaneDom.getBoundingClientRect = jest.fn(() => ({ top: 400, bottom: 700, left: 0, right: 375 }));
-        global.window.requestAnimationFrame = jest.fn((fn) => setTimeout(fn, 20));
+        const firstPaneDom = wrapper.querySelector(`.${prefixTab}-pane`);
+        firstPaneDom.getBoundingClientRect = jest.fn(() => ({
+            top: 100,
+            bottom: 400,
+            left: 0,
+            right: 375,
+        }));
+        const lastPaneDom = wrapper.querySelectorAll(`.${prefixTab}-pane`)[1];
+        lastPaneDom.getBoundingClientRect = jest.fn(() => ({
+            top: 400,
+            bottom: 700,
+            left: 0,
+            right: 375,
+        }));
+        global.window.requestAnimationFrame = jest.fn(fn => setTimeout(fn, 20));
         act(() => {
             jest.advanceTimersByTime(400);
             map.scroll();
@@ -136,46 +178,83 @@ describe('Tabs', () => {
         const onAfterChange = jest.fn();
         const onTouchStopped = jest.fn();
         const ref = createRef();
-        const wrapper = mount(<Tabs
-            tabs={[{ title: 'Title 1' }, { title: 'Title 2' }]}
-            onChange={onChange}
-            onAfterChange={onAfterChange}
-            onTouchStopped={onTouchStopped}
-            swipeable={false}
-            touchSideDisableThreshold={40}
-            stopTouchThreshold={40}
-            ref={ref}
-        >
-            <div className="demo-tab-content">Content 1</div>
-            <div className="demo-tab-content">Content 2</div>
-        </Tabs>);
-        mockContainer(ref, wrapper);
-        expect(wrapper.find(`.${prefixTab}-cell-container`).hasClass('overflow')).toBe(true);
-        const map = mockAddListener(wrapper.find(`.${prefixTab}-pane-container`));
-        wrapper.setProps({ swipeable: true });
+        const { container: wrapper, rerender } = render(
+            <Tabs
+                tabs={[{ title: 'Title 1' }, { title: 'Title 2' }]}
+                onChange={onChange}
+                onAfterChange={onAfterChange}
+                onTouchStopped={onTouchStopped}
+                swipeable={false}
+                touchSideDisableThreshold={40}
+                stopTouchThreshold={40}
+                ref={ref}
+            >
+                <div className="demo-tab-content">Content 1</div>
+                <div className="demo-tab-content">Content 2</div>
+            </Tabs>,
+        );
+        mockContainer(ref);
+        expect(
+            wrapper.querySelector(`.${prefixTab}-cell-container`).classList.contains('overflow'),
+        ).toBe(true);
+        const map = wrapper.querySelector(`.${prefixTab}-pane-container`);
+        rerender(
+            <Tabs
+                tabs={[{ title: 'Title 1' }, { title: 'Title 2' }]}
+                onChange={onChange}
+                onAfterChange={onAfterChange}
+                onTouchStopped={onTouchStopped}
+                swipeable
+                touchSideDisableThreshold={40}
+                stopTouchThreshold={40}
+                onTouchStart={onTouchStart}
+                ref={ref}
+            >
+                <div className="demo-tab-content">Content 1</div>
+                <div className="demo-tab-content">Content 2</div>
+            </Tabs>,
+        );
         // swipe to next tab
-        mockSwipe(map, wrapper, { touchstart: 300, touchmove: 150, touchend: 100 });
+        mockSwipe(map, { touchstart: 300, touchmove: 150, touchend: 100 });
         expect(indexIsActive(wrapper, 1)).toBe(true);
         expect(onChange.mock.calls).toHaveLength(1);
         expect(onChange.mock.calls[0]).toEqual([{ title: 'Title 2' }, 1, 'swipe']);
         expect(onAfterChange.mock.calls).toHaveLength(1);
         // swipe stopped when touch side
-        mockSwipe(map, wrapper, { touchstart: 10, touchmove: 50, touchend: 100 });
+        mockSwipe(map, { touchstart: 10, touchmove: 50, touchend: 100 });
         expect(indexIsActive(wrapper, 1)).toBe(true);
         expect(onChange.mock.calls).toHaveLength(1);
         // swipe stopped when swipe to last tab
-        mockSwipe(map, wrapper, { touchstart: 300, touchmove: 150, touchend: 100 });
+        mockSwipe(map, { touchstart: 300, touchmove: 150, touchend: 100 });
         expect(onTouchStopped.mock.calls).toHaveLength(1);
         expect(onTouchStopped.mock.calls[0]).toEqual([1]);
         // swipe to last tab
-        mockSwipe(map, wrapper, { touchstart: 100, touchmove: 150, touchend: 300 });
+        mockSwipe(map, { touchstart: 100, touchmove: 150, touchend: 300 });
         expect(indexIsActive(wrapper, 0)).toBe(true);
         expect(onChange.mock.calls).toHaveLength(2);
         expect(onAfterChange.mock.calls).toHaveLength(2);
         // use onTouchStart to stop touch
         const onTouchStart = jest.fn(() => true);
-        wrapper.setProps({ onTouchStart });
-        mockSwipe(map, wrapper, { touchstart: 300, touchmove: 150, touchend: 100 });
+        rerender(
+            <Tabs
+                tabs={[{ title: 'Title 1' }, { title: 'Title 2' }]}
+                onChange={onChange}
+                onAfterChange={onAfterChange}
+                onTouchStopped={onTouchStopped}
+                swipeable={false}
+                touchSideDisableThreshold={40}
+                stopTouchThreshold={40}
+                onTouchStart={onTouchStart}
+                ref={ref}
+            >
+                <div className="demo-tab-content">Content 1</div>
+                <div className="demo-tab-content">Content 2</div>
+            </Tabs>,
+        );
+        mockSwipe(map, { touchstart: 300, touchmove: 150, touchend: 100 });
         expect(indexIsActive(wrapper, 0)).toBe(true);
     });
 });
+
+// FIXME: 放最前面用例会不通过，还没仔细研究什么问题
+demoTest('tabs');
