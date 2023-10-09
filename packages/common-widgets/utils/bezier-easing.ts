fa4f6297bed86ad56f452e163f177377bd024ff5
taoyiyue <taoyiyue@bytedance.com>
2023-10-09 17:21:57 +0800

    fix: adjust

--- packages/common-widgets/utils/bezier-easing.ts
+++ packages/common-widgets/utils/bezier-easing.ts
@@ -79,69 +79,6 @@ function LinearEasing(x: number) {
  * @param mY2 {en} The Y coordinate of second point
  * @returns 计算贝塞尔曲线的函数
  * @returns {function} {en} The function of calculating bezier curve
- * @globalVariable
- * ```
- * const NEWTON_ITERATIONS = 4;
- * const NEWTON_MIN_SLOPE = 0.001;
- * const SUBDIVISION_PRECISION = 0.0000001;
- * const SUBDIVISION_MAX_ITERATIONS = 10;
- *
- * const kSplineTableSize = 11;
- * const kSampleStepSize = 1.0 / (kSplineTableSize - 1.0);
- *
- * const float32ArraySupported = typeof Float32Array === 'function';
- *
- * function A(aA1: number, aA2: number) {
- *    return 1.0 - 3.0 * aA2 + 3.0 * aA1;
- * }
- * function B(aA1: number, aA2: number) {
- *    return 3.0 * aA2 - 6.0 * aA1;
- * }
- * function C(aA1: number) {
- *    return 3.0 * aA1;
- * }
-
- * // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
- * function calcBezier(aT: number, aA1: number, aA2: number) {
- *    return ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT;
- * }
-
- * // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
- * function getSlope(aT: number, aA1: number, aA2: number) {
- *    return 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
- * }
-
- * function binarySubdivide(aX: number, aA: number, aB: number, mX1: number, mX2: number) {
- *    let currentX, currentT;
- *    let i = 0;
- *    do {
- *        currentT = aA + (aB - aA) / 2.0;
- *        currentX = calcBezier(currentT, mX1, mX2) - aX;
- *        if (currentX > 0.0) {
- *            aB = currentT;
- *        } else {
- *            aA = currentT;
- *        }
- *    } while (Math.abs(currentX) > SUBDIVISION_PRECISION && ++i < SUBDIVISION_MAX_ITERATIONS);
- *    return currentT;
- * }
- *
- * function newtonRaphsonIterate(aX: number, aGuessT: number, mX1: number, mX2: number) {
- *    for (let i = 0; i < NEWTON_ITERATIONS; ++i) {
- *        const currentSlope = getSlope(aGuessT, mX1, mX2);
- *        if (currentSlope === 0.0) {
- *            return aGuessT;
- *        }
- *        const currentX = calcBezier(aGuessT, mX1, mX2) - aX;
- *        aGuessT -= currentX / currentSlope;
- *    }
- *    return aGuessT;
- * }
-
- * function LinearEasing(x: number) {
- *    return x;
- * }
- * ```
  * @example
  * ```
  * import { bezierEasing } from '@arco-design/mobile-utils';
