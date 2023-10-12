e63559090a79d3cf7a9a1b8e4fc25544a4a297d6
yanyuansiyi <229335832@qq.com>
2023-10-12 14:42:04 +0800

    feat: update notify unit test

--- packages/arcodesign/components/notify/__test__/index.spec.js
+++ packages/arcodesign/components/notify/__test__/index.spec.js
@@ -1,7 +1,54 @@
-import demoTest from '../../../tests/demoTest';
-import mountTest from '../../../tests/mountTest';
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import { defaultContext } from '../../context-provider';
 import Notify from '..';
 
-demoTest('notify');
+describe('Notify work correctly', () => {
+  it('should render notification correctly', () => {
+    render(<Notify visible content="Hello, world!"/>);
+    const contentElement = screen.getByText('Hello, world!');
+    expect(contentElement).toBeInTheDocument()
+  });
 
-mountTest(Notify, 'Notify');
+  it('should call onClose callback after transition when visible is false', () => {
+    const onClose = jest.fn();
+    const { container,rerender } = render(<Notify visible content="Hello, world!" onClose={onClose} />);
+    const notify = container.querySelector(`.${defaultContext.prefixCls}-notify`)
+    rerender(<Notify visible={false} content="Hello, world!" onClose={onClose} />);
+    expect(notify).toHaveStyle('height: 0')
+  });
+});
+
+describe('notify hook work correctly', () => {
+
+  jest.useFakeTimers();
+
+  it('should render notification correctly', () => {
+    const notify = (func, options) => {
+        if (!!window.NotifyInstance) {
+            window.NotifyInstance.close();
+        }
+        window.NotifyInstance = Notify[func](options);
+    };
+    const handleClick=()=>{
+        notify('success', {
+            content: 'Success notification',
+            duration:2000
+        })
+        notify('warn', {
+            content: 'Warning notification',
+        })
+        notify('error', {
+            content: 'Error notification',
+        })
+    }
+    render(<button onClick={handleClick}>Click me</button>);
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+    act(() => {
+      jest.runAllTimers(); 
+    }); 
+  });
+});
