c007e876be98d005584795c603997e9ac24fd784
杨涛 <adaex.yang@bytedance.com>
2024-02-28 12:42:15 +0800

    chore: remove commented out code in Badge component test

--- packages/arcodesign/components/badge/__test__/index.spec.js
+++ packages/arcodesign/components/badge/__test__/index.spec.js
@@ -13,26 +13,6 @@ demoTest('badge');
 mountTest(Badge, 'Badge');
 
 describe('Badge', () => {
-    // it('Render different types', () => {
-    //     const wrapper = mount(<Badge absolute dot />);
-    //     expect(wrapper.find(`.${prefix}.dot.absolute`).length).toBe(1);
-    //     const wrapper1 = mount(<Badge absolute text="12" />);
-    //     expect(wrapper1.find('.badge-text').length).toBe(1);
-    //     expect(wrapper1.text()).toEqual('12');
-    //     const wrapper2 = mount(<Badge absolute text="100" />);
-    //     expect(wrapper2.find('.badge-text').length).toBe(1);
-    //     expect(wrapper2.text()).toEqual('99+');
-    //     const wrapper3 = mount(<Badge absolute text="新" />);
-    //     expect(wrapper3.find('.badge-text').length).toBe(1);
-    //     expect(wrapper3.text()).toEqual('新');
-    //     const wrapper4 = mount(
-    //         <Badge absolute>
-    //             <IconStarFill />
-    //         </Badge>,
-    //     );
-    //     expect(wrapper4.find('svg').length).toBe(1);
-    // });
-
     it('Render different types', () => {
         const { container: container1 } = render(<Badge absolute dot />);
         expect(container1.querySelectorAll(`.${prefix}.dot.absolute`).length).toBe(1);
