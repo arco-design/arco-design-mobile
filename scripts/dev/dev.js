45f8e4015ac988c1a041cc44ffd34620b63b8e9a
黄广 <huangguang.1999@bytedance.com>
2024-02-29 19:23:21 +0800

    refactor: add comment

--- scripts/dev/dev.js
+++ scripts/dev/dev.js
@@ -1,6 +1,8 @@
 const childProcess = require('child_process');
 const { argv } = process;
 
+// 单组件-单 demo 测试命令：npm run dev slider marks
+// 多组件-多 demo 测试命令：npm run dev slider,tabs marks,basic
 childProcess.execSync(
     `cross-env NODE_ENV=development COMPONENTS=${argv[2]} DEMOS=${argv[3]} webpack-dev-server --progress --config scripts/sites/webpack.dev.mobile.js`,
     { stdio: 'inherit' },
