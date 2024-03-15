46ddefc48fb39a51f2767ff384c535c19934b16f
Yiyue Tao <106640896+TinaPeach@users.noreply.github.com>
2024-03-15 17:54:17 +0800

    fix: add icons (#250)

--- packages/arcodesign/components/icon/IconCheck/index.tsx
+++ packages/arcodesign/components/icon/IconCheck/index.tsx
@@ -27,7 +27,6 @@ export default function IconCheck(props: IconProps) {
                         fillRule="evenodd"
                         clipRule="evenodd"
                         d="M16.702 4.47a.5.5 0 00-.705.06L8.33 13.596 3.82 9.724a.5.5 0 00-.705.054l-.652.758a.5.5 0 00.054.706L7.361 15.4a.5.5 0 00.054.053l.526.445.22.188a.5.5 0 00.722-.047l8.641-10.218a.5.5 0 00-.059-.705l-.763-.645z"
-                        fill={useCurrentColor ? 'currentColor' : '#000'}
                     />
                 </svg>
             )}
--- packages/arcodesign/components/icon/IconDownload/index.tsx
+++ packages/arcodesign/components/icon/IconDownload/index.tsx
@@ -7,24 +7,24 @@ export interface IconProps extends SVGAttributes<SVGElement> {
     style?: CSSProperties;
 }
 
-export default function IconUpload(props: IconProps) {
+export default function IconDownload(props: IconProps) {
     const { className = '', useCurrentColor = true, style, ...other } = props;
 
     return (
         <ContextLayout>
             {({ prefixCls }) => (
                 <svg
-                    className={`${prefixCls}-icon ${prefixCls}-icon-upload ${className}`}
+                    className={`${prefixCls}-icon ${prefixCls}-icon-download ${className}`}
                     width="1em"
                     height="1em"
                     style={style}
                     fill={useCurrentColor ? 'currentColor' : '#000'}
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 14 14"
+                    viewBox="0 0 16 16"
                     {...other}
                 >
                     <path
-                        d="M7,1.5l3.1,3.1L9.2,5.4L7.6,3.7v5.9H6.4V3.7L4.8,5.4L3.9,4.6L7,1.5z M2.9,11.4v-1.2H1.8v2.3h10.5	v-2.3h-1.2v1.2H2.9z"
+                        d="M7.3,8.8V1.7h1.3v7.1l1.9-1.9l0.9,0.9L8,11.3L4.5,7.8l0.9-0.9L7.3,8.8z M3.3,13v-1.3H2v2.7h12v-2.7	h-1.3V13H3.3z"
                         fillRule="evenodd"
                         clipRule="evenodd"
                     />
