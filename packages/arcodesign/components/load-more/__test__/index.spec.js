cbf18c1fa3bcace0071748eef415dc182917ac64
41 <yanyuansiyi@bytedance.com>
2024-03-25 19:32:30 +0800

    fix: modify based on comments

--- packages/arcodesign/components/load-more/__test__/index.spec.js
+++ packages/arcodesign/components/load-more/__test__/index.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render, screen,fireEvent } from "@testing-library/react";
-import userEvent from '@testing-library/user-event'
-import '@testing-library/jest-dom'
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 import demoTest from '../../../tests/demoTest';
 import mountTest from '../../../tests/mountTest';
 import LoadMore from '..';
@@ -11,65 +11,76 @@ demoTest('load-more');
 mountTest(LoadMore, 'LoadMore');
 
 describe('LoadMore type', () => {
-
     it('Loading state is rendered correctly', () => {
-        render(<LoadMore status = 'loading' />)
-        const element = screen.getByText('正在努力加载中...')
+        render(<LoadMore status="loading" />);
+        const element = screen.getByText('正在努力加载中...');
         expect(element).toBeInTheDocument();
     });
 
     it('Prepare state is rendered correctly', () => {
-        render(<LoadMore status = 'prepare' />)
-        const element = screen.getByText('上拉加载更多')
+        render(<LoadMore status="prepare" />);
+        const element = screen.getByText('上拉加载更多');
         expect(element).toBeInTheDocument();
     });
 
     it('Nomore state is rendered correctly', () => {
-        render(<LoadMore status = 'nomore' />)
-        const element = screen.getByText('没有更多数据了')
+        render(<LoadMore status="nomore" />);
+        const element = screen.getByText('没有更多数据了');
         expect(element).toBeInTheDocument();
     });
-    
+
     it('Retry state is rendered correctly', () => {
-        render(<LoadMore status = 'retry' />)
-        const element = screen.getByText('加载失败，点击重试')
+        render(<LoadMore status="retry" />);
+        const element = screen.getByText('加载失败，点击重试');
         expect(element).toBeInTheDocument();
     });
 
     it('BeforeReady state is rendered correctly', () => {
-        render(<LoadMore status = 'before-ready' beforeReadyArea = 'before-ready'/>)
-        const element = screen.getByText('before-ready')
+        render(<LoadMore status="before-ready" beforeReadyArea="before-ready" />);
+        const element = screen.getByText('before-ready');
         expect(element).toBeInTheDocument();
     });
 
     it('Default state is rendered correctly', () => {
-        render(<LoadMore status = 'default' />)
-        const element = screen.getAllByRole('generic')
+        render(<LoadMore status="default" />);
+        const element = screen.getAllByRole('generic');
         // 查询的第二个才是正确结果，所以是1
         expect(element[1]).toBeEmptyDOMElement();
     });
-})
+});
 
 describe('LoadMore action', () => {
-
     it('onClick listener correctly', () => {
         const onClick = jest.fn();
-        const { rerender } = render(<LoadMore onClick = {onClick} throttle = {200} trigger = 'click'/>)
-        userEvent.click(screen.getAllByRole('generic')[1])
+        const { rerender } = render(<LoadMore onClick={onClick} throttle={200} trigger="click" />);
+        userEvent.click(screen.getAllByRole('generic')[1]);
         expect(onClick).toBeCalled();
         expect(onClick).toBeCalledTimes(1);
-        rerender(<LoadMore onClick = {onClick} throttle = {200} trigger = 'scroll' status='prepare' getDataAtFirst={false} getDataWhenNoScrollAtFirst/>)
-        userEvent.click(screen.getAllByRole('generic')[1])
+        rerender(
+            <LoadMore
+                onClick={onClick}
+                throttle={200}
+                trigger="scroll"
+                status="prepare"
+                getDataAtFirst={false}
+                getDataWhenNoScrollAtFirst
+            />,
+        );
+        userEvent.click(screen.getAllByRole('generic')[1]);
         expect(onClick).toBeCalled();
         expect(onClick).toBeCalledTimes(2);
     });
 
-    it('onScroll listener correctly',()=>{
+    it('onScroll listener correctly', () => {
         const onChange = jest.fn();
-        const { rerender } = render(<LoadMore status='ready' trigger='scroll' onStatusChange={onChange} threshold={0}/>)
-        rerender(<LoadMore status = 'prepare' trigger='scroll' onStatusChange={onChange} threshold={0}/>)
-        fireEvent.scroll(screen.getAllByRole('generic')[1])
+        const { rerender } = render(
+            <LoadMore status="ready" trigger="scroll" onStatusChange={onChange} threshold={0} />,
+        );
+        rerender(
+            <LoadMore status="prepare" trigger="scroll" onStatusChange={onChange} threshold={0} />,
+        );
+        fireEvent.scroll(screen.getAllByRole('generic')[1]);
         expect(onChange).toBeCalled();
         expect(onChange).toBeCalledTimes(1);
-    })
-})
+    });
+});
--- packages/arcodesign/components/switch/__test__/index.spec.js
+++ packages/arcodesign/components/switch/__test__/index.spec.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { render,fireEvent,waitFor } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import demoTest from '../../../tests/demoTest';
 import mountTest from '../../../tests/mountTest';
-import { createMoveTouchEventObject, createStartTouchEventObject } from '../../../tests/helpers/mockEvent';
+import {
+    createMoveTouchEventObject,
+    createStartTouchEventObject,
+} from '../../../tests/helpers/mockEvent';
 import { defaultContext } from '../../context-provider';
 import Switch from '..';
 
@@ -14,18 +17,22 @@ mountTest(Switch, 'Switch');
 
 describe('Switch', () => {
     it('should render correctly when set as ios/android', () => {
-        const { container: container_ios } = render(<Switch platform="ios" data-testid="switch-ios" />);
-        expect(container_ios.querySelector('.type-ios')).toBeTruthy();
-        const { container: container_android } = render(<Switch platform="android" data-testid="switch-android"/>);
-        expect(container_android.querySelector('.type-android')).toBeTruthy();
+        const { container: containerIos } = render(
+            <Switch platform="ios" data-testid="switch-ios" />,
+        );
+        expect(containerIos.querySelector('.type-ios')).toBeTruthy();
+        const { container: containerAndroid } = render(
+            <Switch platform="android" data-testid="switch-android" />,
+        );
+        expect(containerAndroid.querySelector('.type-android')).toBeTruthy();
     });
-    it('should render and callback correctly when did not use checked',  () => {
+    it('should render and callback correctly when did not use checked', () => {
         const { container } = render(<Switch />);
         const switchElement = container.querySelector(`.${prefix}`);
         expect(switchElement.classList.contains('checked')).toBe(false);
         fireEvent.touchStart(switchElement, createStartTouchEventObject({ x: 100 }));
         fireEvent.touchMove(switchElement, createMoveTouchEventObject({ x: 100 }));
-        fireEvent.touchEnd(switchElement,createMoveTouchEventObject({ x: 100 }));
+        fireEvent.touchEnd(switchElement, createMoveTouchEventObject({ x: 100 }));
         waitFor(() => {
             expect(switchElement.classList.contains('checked')).toBe(true);
         });
@@ -38,11 +45,11 @@ describe('Switch', () => {
         expect(switchElement.classList.contains('checked')).toBe(true);
         fireEvent.touchStart(switchElement, createStartTouchEventObject({ x: 100 }));
         fireEvent.touchMove(switchElement, createMoveTouchEventObject({ x: 120 }));
-        fireEvent.touchEnd(switchElement,createMoveTouchEventObject({ x: 130 }));
+        fireEvent.touchEnd(switchElement, createMoveTouchEventObject({ x: 130 }));
         waitFor(() => {
             expect(switchElement.classList.contains('checked')).toBe(true);
             expect(mockFn).not.toBeCalled();
-        })
+        });
     });
     it('should render correctly when use text', () => {
         const { container } = render(<Switch text={{ on: '关', off: '开' }} />);
@@ -50,11 +57,11 @@ describe('Switch', () => {
         expect(container.querySelector(`.${prefix}-text`).textContent).toBe('关');
         fireEvent.touchStart(switchElement, createStartTouchEventObject({ x: 100 }));
         fireEvent.touchMove(switchElement, createMoveTouchEventObject({ x: 100 }));
-        fireEvent.touchEnd(switchElement,createMoveTouchEventObject({ x: 100 }));
+        fireEvent.touchEnd(switchElement, createMoveTouchEventObject({ x: 100 }));
         expect(container.querySelector(`.${prefix}-text`).textContent).toBe('开');
     });
     it('should render correctly when use shape', () => {
-        const { container,rerender } = render(<Switch shape="semi" />);
+        const { container, rerender } = render(<Switch shape="semi" />);
         expect(container.querySelector(`.${prefix}`).classList.contains('semi')).toBe(true);
         rerender(<Switch />);
         expect(container.querySelector(`.${prefix}`).classList.contains('fully')).toBe(true);
