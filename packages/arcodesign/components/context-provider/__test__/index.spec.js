4f52919c827adcba3bf8bbbaa34b348e1bba7a3b
xiaziqi.sia <xiaziqi.sia@bytedance.com>
2024-04-06 19:29:28 +0800

    test: refactor component tests for `ContextProvider`

--- packages/arcodesign/components/context-provider/__test__/index.spec.js
+++ packages/arcodesign/components/context-provider/__test__/index.spec.js
@@ -1,8 +1,11 @@
+import './matchMedia.mock';
 import React from 'react';
-import { mount } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import mountTest from '../../../tests/mountTest';
 import ContextProvider, { defaultContext } from '..';
+import Button from '../../button';
 import Dialog from '../../dialog';
+import '@testing-library/jest-dom';
 
 const dialogPrefix = `${defaultContext.prefixCls}-dialog`;
 
@@ -10,18 +13,43 @@ mountTest(ContextProvider, 'ContextProvider');
 
 describe('ConfigProvider', () => {
     it('Global configuration takes effect', () => {
-        const wrapper = mount(
-            <ContextProvider
-                system="android">
-                <Dialog
-                    visible={true}
-                    title="title"
-                    footer={[
-                        { content: 'content' },
-                    ]}
-                />
-            </ContextProvider>
+        render(
+            <ContextProvider system="android">
+                <Dialog visible title="title" footer={[{ content: 'content' }]} />
+            </ContextProvider>,
         );
-        expect(wrapper.find(`.${dialogPrefix}-content.${dialogPrefix}-part.android`).length).toBe(1);
+        expect(
+            document.querySelectorAll(`.${dialogPrefix}-content.${dialogPrefix}-part.android`)
+                .length,
+        ).toBe(1);
+    });
+    it('Set theme correctly', () => {
+        const createElementSpy = jest
+            .spyOn(document.documentElement, 'appendChild')
+            .mockReturnValueOnce(document.createElement('style'));
+        jest.spyOn(document.documentElement, 'removeChild').mockImplementation(jest.fn);
+        render(
+            <ContextProvider theme={{ a: 1 }}>
+                <Button>content</Button>
+            </ContextProvider>,
+        );
+        expect(createElementSpy.mock.calls[0][0].innerHTML).toBe(':root {--a: 1;}');
+        jest.clearAllMocks();
+    });
+    it('isDarkMode takes effect', () => {
+        render(
+            <ContextProvider isDarkMode theme={{ abcde: 1 }}>
+                <Dialog visible title="title" footer={[{ content: 'content' }]} />
+            </ContextProvider>,
+        );
+        expect(document.body).toHaveClass('arco-theme-dark');
+    });
+    it('useDarkMode takes effect', () => {
+        render(
+            <ContextProvider useDarkMode>
+                <Button>content</Button>
+            </ContextProvider>,
+        );
+        expect(document.body).toHaveClass('arco-theme-dark');
     });
 });
