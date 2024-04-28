b25836dffeb6bb22bc91f1bdcdbef6c61a3a7c73
taoyiyue <taoyiyue@bytedance.com>
2024-04-28 19:09:55 +0800

    fix: pc site vue radio

--- sites/pc/entry/nav/radio.tsx
+++ sites/pc/entry/nav/radio.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Radio } from 'arco';
+import { getUrlsByLanguage } from '../../../utils/url';
+import { LanguageSupport } from '../../../utils/language';
+
+const isVue = window.location.pathname.includes('vue');
 
 const RadioBox: React.FC = function () {
-    return null;
+    function onChange(value) {
+        window.open(getUrlsByLanguage(LanguageSupport.CH, value === 'Vue').DOC_SITE);
+    }
+
+    return (
+        <div className="arcodesign-pc-menu-header">
+            <Radio.Group
+                onChange={onChange}
+                size="large"
+                value={isVue ? 'Vue' : 'React'}
+                type="button"
+                options={['React', 'Vue']}
+            />
+        </div>
+    );
 };
 
 export default RadioBox;
