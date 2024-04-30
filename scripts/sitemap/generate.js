2226bf740b3304ebda0b99569334f308dfaf5f94
杨涛 <adaex.yang@bytedance.com>
2024-04-30 11:43:36 +0800

    feat: add txt

--- scripts/sitemap/generate.js
+++ scripts/sitemap/generate.js
@@ -16,13 +16,16 @@ function createSitemap(dir, reg, outputFile) {
     walkDir(dir, function (filePath) {
         if (filePath.match(reg)) {
             // 将文件路径转换为链接
-            const link = `https://raw.githubusercontent.com/arco-design/arco-design-mobile/main/${filePath}`;
-            sitemap.push(`<div><a href="${link}">${filePath}</a></div>`);
+            // const link = `https://raw.githubusercontent.com/arco-design/arco-design-mobile/main/${filePath}`;
+            const link = `https://cdn.jsdelivr.net/gh/arco-design/arco-design-mobile/${filePath}`;
+            // sitemap.push(`<div><a href="${link}">${filePath}</a></div>`);
+            sitemap.push(link);
         }
     });
-    sitemap.unshift('<html><body><ul>');
-    sitemap.push('</ul></body></html>');
+    // sitemap.unshift('<html><body><ul>');
+    // sitemap.push('</ul></body></html>');
 
+    fs.mkdirSync(path.dirname(outputFile), { recursive: true });
     // 将链接写入Markdown文件
     fs.writeFileSync(outputFile, sitemap.join('\n'));
 }
@@ -31,6 +34,6 @@ function createSitemap(dir, reg, outputFile) {
 const targetDir = './packages/arcodesign/components/';
 
 // 执行函数
-createSitemap(targetDir, /.(tsx)$/, `sites/sitemap/react-source.html`);
-createSitemap(targetDir, /.(less)$/, `sites/sitemap/react-style.html`);
-createSitemap(targetDir, /demo\/\w+\.md$/, `sites/sitemap/react-demo.html`);
+createSitemap(targetDir, /.(tsx)$/, `sites/sitemap/react-source.txt`);
+createSitemap(targetDir, /.(less)$/, `sites/sitemap/react-style.txt`);
+createSitemap(targetDir, /demo\/\w+\.md$/, `sites/sitemap/react-demo.txt`);
