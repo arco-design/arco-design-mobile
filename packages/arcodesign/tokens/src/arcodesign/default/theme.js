864ca11a5a19d1b4330dcd521666d17147c5d03e
吴霞玉 <wuxiayu@bytedance.com>
2024-07-01 19:51:34 +0800

    fix: stepper style

---
+++ packages/arcodesign/tokens/src/arcodesign/default/theme.js
@@ -0,0 +1,223 @@
+/** auto-generated */
+exports.theme = {
+    "arcom-color-bg-1": {
+        "light": "#FFFFFF",
+        "dark": "#17171A"
+    },
+    "arcom-color-bg-2": {
+        "light": "#FFFFFF",
+        "dark": "#232324"
+    },
+    "arcom-color-bg-3": {
+        "light": "#FFFFFF",
+        "dark": "#2A2A2B"
+    },
+    "arcom-color-fill-2": {
+        "light": "#F7F8FA",
+        "dark": "hsla(0, 0%, 100%, 0.08)"
+    },
+    "arcom-color-white-1": {
+        "light": "#FFFFFF",
+        "dark": "rgba(255, 255, 255, 0.9)"
+    },
+    "arcom-color-arcoblue-1": {
+        "light": "#E8F3FF",
+        "dark": "#000D4D"
+    },
+    "arcom-color-arcoblue-2": {
+        "light": "#BEDAFF",
+        "dark": "#041B79"
+    },
+    "arcom-color-arcoblue-3": {
+        "light": "#94BFFF",
+        "dark": "#0E32A6"
+    },
+    "arcom-color-arcoblue-4": {
+        "light": "#6AA1FF",
+        "dark": "#1D4DD2"
+    },
+    "arcom-color-arcoblue-5": {
+        "light": "#4080FF",
+        "dark": "#306FFF"
+    },
+    "arcom-color-arcoblue-6": {
+        "light": "#165DFF",
+        "dark": "#3C7EFF"
+    },
+    "arcom-color-arcoblue-7": {
+        "light": "#0E42D2",
+        "dark": "#689FFF"
+    },
+    "arcom-color-arcoblue-8": {
+        "light": "#072CA6",
+        "dark": "#93BEFF"
+    },
+    "arcom-color-arcoblue-9": {
+        "light": "#031A79",
+        "dark": "#BEDAFF"
+    },
+    "arcom-color-arcoblue-10": {
+        "light": "#000D4D",
+        "dark": "#EAF4FF"
+    },
+    "arcom-color-gray-1": {
+        "light": "#f7f8fa",
+        "dark": "#17171a"
+    },
+    "arcom-color-gray-2": {
+        "light": "#f2f3f5",
+        "dark": "#2e2e30"
+    },
+    "arcom-color-gray-3": {
+        "light": "#e5e6eb",
+        "dark": "#484849"
+    },
+    "arcom-color-gray-4": {
+        "light": "#c9cdd4",
+        "dark": "#5f5f60"
+    },
+    "arcom-color-gray-5": {
+        "light": "#a9aeb8",
+        "dark": "#78787a"
+    },
+    "arcom-color-gray-6": {
+        "light": "#86909c",
+        "dark": "#929293"
+    },
+    "arcom-color-gray-7": {
+        "light": "#6b7785",
+        "dark": "#ababac"
+    },
+    "arcom-color-gray-8": {
+        "light": "#4e5969",
+        "dark": "#c5c5c5"
+    },
+    "arcom-color-gray-9": {
+        "light": "#272e3b",
+        "dark": "#dfdfdf"
+    },
+    "arcom-color-gray-10": {
+        "light": "#1d2129",
+        "dark": "#f6f6f6"
+    },
+    "arcom-color-green-1": {
+        "light": "#E8FFEA",
+        "dark": "#004D1C"
+    },
+    "arcom-color-green-2": {
+        "light": "#AFF0B5",
+        "dark": "#046625"
+    },
+    "arcom-color-green-3": {
+        "light": "#7BE188",
+        "dark": "#0A802D"
+    },
+    "arcom-color-green-4": {
+        "light": "#4CD263",
+        "dark": "#129A37"
+    },
+    "arcom-color-green-5": {
+        "light": "#23C343",
+        "dark": "#1DB440"
+    },
+    "arcom-color-green-6": {
+        "light": "#00B42A",
+        "dark": "#27C346"
+    },
+    "arcom-color-green-7": {
+        "light": "#009A29",
+        "dark": "#50D266"
+    },
+    "arcom-color-green-8": {
+        "light": "#008026",
+        "dark": "#7EE18B"
+    },
+    "arcom-color-green-9": {
+        "light": "#006622",
+        "dark": "#B2F0B7"
+    },
+    "arcom-color-green-10": {
+        "light": "#004D1C",
+        "dark": "#EBFFEC"
+    },
+    "arcom-color-orange-1": {
+        "light": "#FFF7E8",
+        "dark": "#4D1B00"
+    },
+    "arcom-color-orange-2": {
+        "light": "#FFE4BA",
+        "dark": "#793004"
+    },
+    "arcom-color-orange-3": {
+        "light": "#FFCF8B",
+        "dark": "#A64B0A"
+    },
+    "arcom-color-orange-4": {
+        "light": "#FFB65D",
+        "dark": "#D26913"
+    },
+    "arcom-color-orange-5": {
+        "light": "#FF9A2E",
+        "dark": "#FF8D1F"
+    },
+    "arcom-color-orange-6": {
+        "light": "#FF7D00",
+        "dark": "#FF9626"
+    },
+    "arcom-color-orange-7": {
+        "light": "#D25F00",
+        "dark": "#FFB357"
+    },
+    "arcom-color-orange-8": {
+        "light": "#A64500",
+        "dark": "#FFCD87"
+    },
+    "arcom-color-orange-9": {
+        "light": "#792E00",
+        "dark": "#FFE3B8"
+    },
+    "arcom-color-orange-10": {
+        "light": "#4D1B00",
+        "dark": "#FFF7E8"
+    },
+    "arcom-color-red-1": {
+        "light": "#FFECE8",
+        "dark": "#4D000A"
+    },
+    "arcom-color-red-2": {
+        "light": "#FDCDC5",
+        "dark": "#770611"
+    },
+    "arcom-color-red-3": {
+        "light": "#FBACA3",
+        "dark": "#A1161F"
+    },
+    "arcom-color-red-4": {
+        "light": "#F98981",
+        "dark": "#CB2E34"
+    },
+    "arcom-color-red-5": {
+        "light": "#F76560",
+        "dark": "#F54E4E"
+    },
+    "arcom-color-red-6": {
+        "light": "#F53F3F",
+        "dark": "#F76965"
+    },
+    "arcom-color-red-7": {
+        "light": "#CB272D",
+        "dark": "#F98D86"
+    },
+    "arcom-color-red-8": {
+        "light": "#A1151E",
+        "dark": "#FBB0A7"
+    },
+    "arcom-color-red-9": {
+        "light": "#770813",
+        "dark": "#FDD1CA"
+    },
+    "arcom-color-red-10": {
+        "light": "#4D000A",
+        "dark": "#FFF0EC"
+    }
+}
