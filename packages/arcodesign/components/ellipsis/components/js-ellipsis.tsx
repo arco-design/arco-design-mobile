def42ec196de8eaba7379025fa2c048c211c130d
tangshumin <tangshumin.666@bytedance.com>
2025-05-12 19:52:48 +0800

    feat: fix emoji truncation issue abute `Ellipsis`

--- packages/arcodesign/components/ellipsis/components/js-ellipsis.tsx
+++ packages/arcodesign/components/ellipsis/components/js-ellipsis.tsx
@@ -68,7 +68,7 @@ const JsEllipsis = forwardRef((props: JsEllipsisProps, ref: Ref<JsEllipsisRef>) 
             }
         }
         // Remove the last character if it has a emoji character.
-        currentText = currentText.replace(/[\uD800-\uDBFF]+$/g, '');
+        currentText = currentText.replace(/[\uD800-\uDBFF]+$/, '');
         // Remove the exclude char at the end of the content.
         while (endExcludes && endExcludes.includes(currentText[currentText.length - 1])) {
             currentText = currentText.slice(0, -1);
