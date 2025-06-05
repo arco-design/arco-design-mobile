8c2edf7fb43f6e7e0d31b9ecd68bf29dc4824798
杨涛 <adaex.yang@bytedance.com>
2025-06-05 20:51:52 +0800

    feat: 修复加载组件导入顺序，确保样式和功能正常

--- packages/arcodesign-vue/components/components.ts
+++ packages/arcodesign-vue/components/components.ts
@@ -1,16 +1,16 @@
 import badgeDefault from './badge';
 import cellDefault from './cell';
 import contextProviderDefault from './context-provider';
+import loadingDefault from './loading';
 import notifyDefault from './notify';
 import transitionDefault from './transition';
-import loadingDefault from './loading';
 
 export * from './badge';
 export * from './cell';
 export * from './context-provider';
+export * from './loading';
 export * from './notify';
 export * from './transition';
-export * from './loading';
 
 export const allCompInstall = [
     badgeDefault.install,
