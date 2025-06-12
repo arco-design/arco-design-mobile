47d0b010a8dc68a8af885d8f631350dd800ed07d
杨涛 <adaex.yang@bytedance.com>
2025-06-12 16:28:45 +0800

    test: 移除 demoTest 函数中的 waitTimers 参数及相关逻辑

--- packages/arcodesign/tests/demoTest.js
+++ packages/arcodesign/tests/demoTest.js
@@ -8,10 +8,9 @@ import path from 'path';
  * @description 测试组件的 demo 是否能正确渲染
  * @param {string} comp 组件名称
  * @param {boolean} useFakeTimers 是否使用 fake timers
- * @param {boolean} waitTimers 是否等待所有挂起的定时器，有一些场景下需要等待定时器执行完毕，例如用了 nextTick 或 setTimeout 等异步操作，防止 unmount 后仍然执行代码而报错
  * @returns {void}
  */
-export default function demoTest(comp, { useFakeTimers = false, waitTimers = false } = {}) {
+export default function demoTest(comp, { useFakeTimers = false } = {}) {
     describe(`${comp} demo test`, () => {
         beforeEach(() => {
             if (useFakeTimers) {
@@ -20,12 +19,6 @@ export default function demoTest(comp, { useFakeTimers = false, waitTimers = fal
         });
 
         afterEach(() => {
-            // 如果设置了 waitTimers，运行所有挂起的定时器
-            if (waitTimers) {
-                act(() => jest.runAllTimers());
-                jest.clearAllTimers();
-            }
-            // 如果使用了 fake timers，确保在测试结束时清除定时器
             if (useFakeTimers) {
                 jest.useRealTimers();
             }
@@ -37,11 +30,6 @@ export default function demoTest(comp, { useFakeTimers = false, waitTimers = fal
             it(`${comp} demo: ${filename} renders correctly`, () => {
                 const demo = require(file).default;
                 const { asFragment, unmount } = render(React.createElement(demo));
-
-                // 如果使用了 fake timers，并且需要等待定时器，运行所有挂起的定时器
-                if (useFakeTimers && waitTimers) {
-                    act(() => jest.runAllTimers());
-                }
 
                 // 检查快照
                 // 这里的 asFragment() 返回一个 DocumentFragment，包含了组件的渲染结果
