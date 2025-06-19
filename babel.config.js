441250cec50ec2b9f2d782843085abf0f1ad85d4
Aex <adaex.yang@bytedance.com>
2025-06-19 06:45:23 +0000

    feat: add support for private methods and properties in Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -24,6 +24,8 @@ const env = {
 const plugins = [
     '@babel/plugin-transform-runtime',
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
 ];
 
 module.exports = { presets, plugins, env };
